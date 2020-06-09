c836a5e8f4441cf8d7539da7ac92d63315a10a33
Telly Lowe <57466776+TellyLowe@users.noreply.github.com>
2020-06-09 15:21:26 -0400

    reverted

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,16 @@ $(function () {
             $("#carouselButton").children("i").removeClass("fa-pause");
             $("#carouselButton").children("i").addClass("fa-play");
         } else {
-              $(".carousel").carousel("cycle");
-              $("#carouselButton").children("i").removeClass("fa-play");
-              $("#carouselButton").children("i").addClass("fa-pause");
+                $(".carousel").carousel("cycle");
+                $("#carouselButton").children("i").removeClass("fa-play");
+                $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
     $("#reserveButton").click(function (){
-              $("#reserveModal").modal("show")
+                $("#reserveModal").modal("show")
     });
     $("#loginButton").click(function (){
-             $("#loginModal").modal("show")
+                $("#loginModal").modal("show")
     });
 });
 
